8513ded002aac9c28a8bb0361fa0e10023bfe2a5
Patrick McParland <patrick.mcparland@seopa.com>
2015-12-30 13:57:22 +0000

    Updates for question list

---
+++ src/test/java/TestCases/ExcelTestCaseTest.java
@@ -0,0 +1,28 @@
+package TestCases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by patrickmcparland on 30/12/2015.
+ */
+public class ExcelTestCaseTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+}
