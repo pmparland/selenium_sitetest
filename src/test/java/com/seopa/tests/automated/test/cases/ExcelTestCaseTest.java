6dbd8abe3749a35cf13517e1b047ae8ae868a2a9
pmcparland <patrick.mcparland@seopa.com>
2016-01-07 15:01:27 +0000

    Added test cases for Home

--- src/test/java/com/seopa/tests/automated/test/cases/ExcelTestCaseTest.java
+++ src/test/java/com/seopa/tests/automated/test/cases/ExcelTestCaseTest.java
@@ -11,7 +11,8 @@ import static org.testng.Assert.*;
  */
 public class ExcelTestCaseTest {
 
-    private static String ADDRESS = "https://car-insurance.quotezone.co.uk/car/index.php?page=1";
+    private static String CAR_ADDRESS = "https://car-insurance.quotezone.co.uk/car/index.php?page=1";
+    private static String HOME_ADDRESS = "https://home-insurance.quotezone.co.uk/home/";
     private static String FIREFOX = "FireFox";
     private static String CHROME = "Chrome"; //For chrome you need to install the chrome driver
     private static String IE = "IE"; //For chrome you need to install the IE driver
@@ -28,7 +29,7 @@ public class ExcelTestCaseTest {
     }
 
     @Test
-    public void testExecuteCarQuote() throws Exception {
+    public void testExecuteSimpleCarQuote() throws Exception {
         //Get current working directory
         String workingDir=System.getProperty("user.dir");
         String separator = System.getProperty("file.separator");
@@ -36,7 +37,19 @@ public class ExcelTestCaseTest {
 
         ExcelTestCase test = new ExcelTestCase();
 
-        assertTrue(test.execute(excelFile, ADDRESS, FIREFOX));
+        assertTrue(test.execute(excelFile, CAR_ADDRESS, FIREFOX));
+    }
+
+    @Test
+    public void testExecuteComplexCarQuote() throws Exception {
+        //Get current working directory
+        String workingDir=System.getProperty("user.dir");
+        String separator = System.getProperty("file.separator");
+        String excelFile = workingDir+separator+"src"+separator+"test"+separator+"java"+separator+ "com" + separator +"seopa"+separator+"tests"+separator+"automated"+separator+"test"+separator+"cases"+separator+ "ComplexCarJourney01.xlsx";
+
+        ExcelTestCase test = new ExcelTestCase();
+
+        assertTrue(test.execute(excelFile, CAR_ADDRESS, FIREFOX));
     }
 
     @Test
@@ -48,7 +61,7 @@ public class ExcelTestCaseTest {
 
         ExcelTestCase test = new ExcelTestCase();
 
-        assertTrue(test.execute(excelFile, ADDRESS, FIREFOX));
+        assertTrue(test.execute(excelFile, CAR_ADDRESS, FIREFOX));
     }
 
     @Test
@@ -60,6 +73,18 @@ public class ExcelTestCaseTest {
 
         ExcelTestCase test = new ExcelTestCase();
 
-        assertTrue(test.execute(excelFile, ADDRESS, FIREFOX));
+        assertTrue(test.execute(excelFile, CAR_ADDRESS, FIREFOX));
+    }
+
+    @Test
+    public void testHomeSimpleQuote01() throws Exception {
+        //Get current working directory
+        String workingDir=System.getProperty("user.dir");
+        String separator = System.getProperty("file.separator");
+        String excelFile = workingDir+separator+"src"+separator+"test"+separator+"java"+separator+ "com" + separator +"seopa"+separator+"tests"+separator+"automated"+separator+"test"+separator+"cases"+separator+ "SimpleHomeQuote01.xlsx";
+
+        ExcelTestCase test = new ExcelTestCase();
+
+        assertTrue(test.execute(excelFile, HOME_ADDRESS, FIREFOX));
     }
 }
